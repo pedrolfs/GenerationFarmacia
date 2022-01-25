2488f4c60ef499bba3f10099fe568e082c01f874
Edilaine <edilaine45@yahoo.com.br>
2022-01-25 17:46:59 -0300

    @Edilainesds [Produto Controller add metodo delete]

--- src/main/java/com/generation/farmacia/controller/ProdutoController.java
+++ src/main/java/com/generation/farmacia/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,11 @@ public class ProdutoController {
 	@PutMapping
 	public ResponseEntity<Produto> put(@RequestBody Produto produto) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 
 }

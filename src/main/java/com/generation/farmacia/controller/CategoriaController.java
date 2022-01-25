0bdad72ebaab4cbcd0ef314dfb015ee251236105
Pedro Lucas <peedrolucass@outlook.com>
2022-01-25 14:42:25 -0300

    @pdrlcx [Acrescentando PUT, POST, DELETE Controller Categoria]

--- src/main/java/com/generation/farmacia/controller/CategoriaController.java
+++ src/main/java/com/generation/farmacia/controller/CategoriaController.java
@@ -2,13 +2,15 @@ package com.generation.farmacia.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.generation.farmacia.model.Categoria;
 import com.generation.farmacia.repository.CategoriaRepository;
 
-/**@author Cesar
+/**@author Cesar, Pedro Lucas
  * @version v1 - Desenvolvimento Controller Categoria (adição de getAll, getbyId e getNomecategoria)
  * @since 25/01/2022 
  * */ 
@@ -28,5 +30,20 @@ public class CategoriaController {
 	
 	public ResponseEntity<List<Categoria>> GetByNomecategoria (@PathVariable String nomecategoria) {
 		return ResponseEntity.ok(repository.findAllByNomeCategoriaCotainingIgnoreCase(nomecategoria));
+	}
+	
+	
+	public ResponseEntity<Categoria> post(@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+	
+	
+	public ResponseEntity<Categoria> put(@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
+	}
+	
+	
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }

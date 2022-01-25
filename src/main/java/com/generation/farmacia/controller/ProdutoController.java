0694d7eabb445b8be0575177c37e3768b35c9626
Edilaine <edilaine45@yahoo.com.br>
2022-01-25 17:45:07 -0300

    @Edilainesds [Produto Controller add metodo Put

--- src/main/java/com/generation/farmacia/controller/ProdutoController.java
+++ src/main/java/com/generation/farmacia/controller/ProdutoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,11 @@ public class ProdutoController {
 	@PostMapping
 	public ResponseEntity<Produto> post(@RequestBody Produto produto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
+
+	@PutMapping
+	public ResponseEntity<Produto> put(@RequestBody Produto produto) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
 	}
 
 }

65e8470e02700eb294ea6e0a0a76ed8cf0a1d0c9
Edilaine <edilaine45@yahoo.com.br>
2022-01-25 17:40:58 -0300

    @Edilainesds [Produto Controller, métodos Post]

--- src/main/java/com/generation/farmacia/controller/ProdutoController.java
+++ src/main/java/com/generation/farmacia/controller/ProdutoController.java
@@ -3,13 +3,17 @@ package com.generation.farmacia.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.farmacia.model.Categoria;
 import com.generation.farmacia.model.Produto;
 import com.generation.farmacia.repository.ProdutoRepository;
 
@@ -43,6 +47,9 @@ public class ProdutoController {
 		return ResponseEntity.ok(repository.findAllByNomeProdutoContainingIgnoreCase(nome));
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<Produto> post(@RequestBody Produto produto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
 
 }
